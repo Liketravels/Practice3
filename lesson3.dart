6935e5632e03f80efc311a85d344a3301ff3d716
Liketravels <liketravelss@gmail.com>
2023-03-31 17:27:57 +0300

    Practice3

---
+++ lesson3.dart
@@ -0,0 +1,77 @@
+
+// void main() {
+// // int a = 20;
+// // int b = 10;
+// // b += a;  /// b = b + a
+// // b -= a; /// b = b-a
+
+
+// // print("b = $b");
+// // b *= a; /// b = b*a
+// // print("");
+
+// //b ~/= a; // b = b / a
+
+// // print(a + b);
+// // print(a * b);
+// // print(a / b);
+
+// // int a=20 b=10
+// // bool isTrue = a > b;
+// // print(isTrue);
+// // print(a < b); //false
+// // print(a > b);  //true
+// // print(a >= b);  //true
+// // print(a <= b); //true
+// // print(a == b); // false
+// // print(a != b); //true
+
+
+// int a = 12;
+// int b = 15;
+
+// bool isTrue = a > b;
+//  print(isTrue);
+//  print(a > b); //false
+//  print(a < b);  //true
+//  print(a <= b);  //true
+//  print(a >= b); //true
+//  print(a == b); // false
+ 
+
+// int j = 2;
+// j +=j;
+// print(j);
+
+// int c = 5;
+// int t = 10;
+// int f = 3;
+// int d = 16;
+// int s = 18;
+// ``
+// print(c-t);
+// print(d*f);
+// print(s/j);
+
+
+//  // var h = "Vitaly";
+//   //print(h is! int);
+//   //String h = 10 as String;
+
+// int ax = 5;
+// int bx = 10;
+// print(ax > bx && bx < ax); // false && false or true true
+// // 
+
+// print(ax.isEven);
+// print(ax.isOdd);
+// print(ax.compareTo(10));
+
+// int az = 5;
+// double bz = 2.5;
+// print("5 + 2.5 = ${a + b}");
+
+
+
+// }
+
---
+++ practise3.dart
@@ -0,0 +1,45 @@
+void main() {
+//  int a = 5;
+//  double b = 5.126;
+//  print(a + b);
+//  print(a - b);
+// print(a * b);
+// print(a / b);
+// print("a + b = ${a + b}");
+// print("a - b = ${a - b}");
+// print("a * b = ${a * b}");
+// print("a / b = ${a / b}");
+
+
+double a = 5;
+double b = 2;
+double res = 0;
+res = a + b;
+print("a + b = ${res.toInt()}");
+res= a - b;
+print("a - b = ${res.round()}");
+
+ a = 132;
+ b = 123;
+ res= a * b;
+print("a * b = ${res.round()}");
+print("a * b = ${a * b}");
+
+ a = 78;
+ b = 2;
+ res= a / b;
+print("a / b = ${res.round()}");
+print("a / b = ${a / b}");
+
+ a = 78;
+ b = 2.5;
+ res= a / b;
+print("a /b = ${res.round()}");
+print("a / b = ${a / b}");
+
+ a = 0;
+ b = 5;
+ res= a * b;
+print("a * b = ${res.round()}");
+print("a * b = ${a * b}");
+}
